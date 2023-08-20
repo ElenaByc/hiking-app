fed6a6bd35053097a0540d17209d58fcdc678ea2
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-08-19 20:39:51 -0700

    Create User entity

---
+++ src/main/java/com/elenabyc/hikingapp/entities/User.java
@@ -0,0 +1,38 @@
+package com.elenabyc.hikingapp.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "Users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "user_name")
+    private String username;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column
+    private String password;
+
+//    public User(UserDto userDto) {
+//        if (userDto.getUsername() != null) {
+//            this.username = userDto.getUsername();
+//        }
+//        if (userDto.getPassword() != null) {
+//            this.password = userDto.getPassword();
+//        }
+//    }
+}
+

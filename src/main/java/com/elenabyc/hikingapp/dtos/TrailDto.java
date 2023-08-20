16000b29201ec7822093520b778092ec5d08d8e7
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-08-19 21:58:06 -0700

    Create TrailDto

--- src/main/java/com/elenabyc/hikingapp/dtos/TrailDto.java
+++ src/main/java/com/elenabyc/hikingapp/dtos/TrailDto.java
@@ -1,4 +1,36 @@
 package com.elenabyc.hikingapp.dtos;
 
-public class TrailDto {
+import com.elenabyc.hikingapp.entities.Trail;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TrailDto implements Serializable {
+    private Long id;
+    private String name;
+    private String alias;
+    private String image;
+    private Set<ReviewDto> reviewsDto = new HashSet<>();
+
+    public TrailDto(Trail trail) {
+        if (trail.getId() != null) {
+            this.id = trail.getId();
+        }
+        if (trail.getName() != null) {
+            this.name = trail.getName();
+        }
+        if (trail.getAlias() != null) {
+            this.alias = trail.getAlias();
+        }
+        if (trail.getImage() != null) {
+            this.image = trail.getImage();
+        }
+    }
 }

b6b5200906beea4ebe3b56ea5fd8ac361534c40b
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-09-30 21:14:42 -0700

    feat: add form to edit review to the user's reviews page

--- src/main/resources/static/js/reviews.js
+++ src/main/resources/static/js/reviews.js
@@ -47,18 +47,60 @@ const createReviewCard = (review) => {
 
   const reviewDate = document.createElement('p');
   reviewDate.classList.add('review-date');
+  reviewDate.setAttribute('id', `review-date-${review.id}`);
   reviewDate.innerText = `Date: ${review.date}`;
   divInfo.appendChild(reviewDate);
 
   const reviewRating = document.createElement('p');
   reviewRating.classList.add('review-rating');
+  reviewRating.setAttribute('id', `review-rating-${review.id}`);
   reviewRating.innerText = `Rating: ${review.rating}`;
   divInfo.appendChild(reviewRating);
 
   const reviewBody = document.createElement('p');
   reviewBody.classList.add('review-body');
+  reviewBody.setAttribute('id', `review-body-${review.id}`)
   reviewBody.innerText = review.body;
   divInfo.appendChild(reviewBody);
+
+
+  const editReviewForm = document.createElement('form');
+  editReviewForm.setAttribute('id', `edit-form-${review.id}`);
+  editReviewForm.classList.add('edit-review-from');
+  editReviewForm.style.display = 'none';
+  const editFormHeader = document.createElement('h4');
+  editFormHeader.innerText = 'Edit review';
+  editReviewForm.appendChild(editFormHeader);
+  const div1 = document.createElement('div');
+  div1.classList.add('mb-3');
+  const textarea = document.createElement('textarea');
+  textarea.classList.add('form-control');
+  textarea.setAttribute('id', `edit-review-body-${review.id}`);
+  textarea.setAttribute('rows', '7');
+  textarea.setAttribute('cols', '50');
+  textarea.innerText = `${review.body}`;
+  div1.appendChild(textarea);
+  editReviewForm.appendChild(div1);
+
+  // reviewEditForm.innerHTML = `
+  //           <div class="mb-3">
+  //             <textarea id="review-body" class="form-control" rows="7" cols="50"
+  //               placeholder="Type your review here..." required></textarea>
+  //           </div>
+  //           <select class="form-select form-select-md mb-3" name="rating" id="rating" required>
+  //             <option value="" selected disabled>--Select your rating--</option>
+  //             <option value="1">1 - Not good</option>
+  //             <option value="2">2 - Could’ve been better</option>
+  //             <option value="3">3 - OK</option>
+  //             <option value="4">4 - Good</option>
+  //             <option value="5">5 - Great!</option>
+  //           </select>
+  //           <div class="mb-3 review-btns">
+  //             <input type="submit" class="button" value="Submit review">
+  //             <button class="button" id="revew-cancel-btn">Cancel</button>
+  //           </div>`;
+
+  divInfo.appendChild(editReviewForm);
   divContent.appendChild(divInfo);
 
   const divBtns = document.createElement('div');
@@ -96,9 +138,25 @@ const handleDeleteReview = async (e) => {
 }
 
 const handleEditReview = async (e) => {
-  // const trailCard = e.target.closest('.trail-card');
-  // const trailId = Number(e.target.id.substring(11));
-  // console.log(trailId);
+  const reviewId = Number(e.target.id.substring(9));
+  console.log(reviewId);
+  const reviewBody = document.querySelector(`#review-body-${reviewId}`);
+  const reviewRating = document.querySelector(`#review-rating-${reviewId}`);
+  const reviewDate = document.querySelector(`#review-date-${reviewId}`);
+  const editReviewForm = document.querySelector(`#edit-form-${reviewId}`);
+  editReviewForm.style.display = 'block';
+  const textarea = document.querySelector(`#edit-review-body-${reviewId}`);
+  console.log(textarea);
+  textarea.focus();
+  textarea.setSelectionRange(textarea.value.length, textarea.value.length);
+  reviewBody.style.display = 'none';
+  reviewRating.style.display = 'none';
+  reviewDate.style.display = 'none';
+  document.querySelector(`#edit-btn-${reviewId}`).disabled = true;
+
+
+
+
   // trailCard.style.display = 'none';
   // const response = await fetch(`/api/users/trails/${userId}/remove/${trailId}`, {
   //   method: 'POST',

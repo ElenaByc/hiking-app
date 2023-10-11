a71ee1b267ba974cd6c603b9feddbe4da31aec12
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-10-11 16:33:35 -0700

    feat: create Picture entity

---
+++ src/main/java/com/elenabyc/hikingapp/entities/Picture.java
@@ -0,0 +1,32 @@
+package com.elenabyc.hikingapp.entities;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "pictures")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Picture {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "picture_id")
+    private Long id;
+
+    @Column(name = "picture_url")
+    private String url;
+
+    @ManyToOne
+    @JsonBackReference
+    private User user;
+
+    @ManyToOne
+    @JsonBackReference
+    private Trail trail;
+}
